5a23de6f0f2eb7f858ba9234dae28b9d0a35b1c4
michelleyeoh <michellew.yeoh@gmail.com>
2024-10-07 13:28:46 -0700

    finished desktop view

--- app/_components/welcome/quickLinkCard.jsx
+++ app/_components/welcome/quickLinkCard.jsx
@@ -4,10 +4,14 @@ import Image from 'next/image';
 export function QuickLinkCard({ image, title }){
     return(
         <div className={styles.link_card}>
-            <Image src={image} alt={image} width={50} height={50}/>
-            <p><b>{title}</b></p>
-            <br/>
-            <button className={styles.link_button}>View More →</button>
+            <Image className={styles.folder_image} src="/welcome/folder_tab.svg" alt="folder_tab" width={300} height={300}/>
+            <div className={styles.link_content}>
+                <Image src={image} alt={image} width={50} height={50}/>
+                <p><b>{title}</b></p>
+                <br/>
+                <button className={styles.link_button}>View More →</button>
+            </div>
+            
         </div>
     );
 }
--- app/_components/welcome/welcome.jsx
+++ app/_components/welcome/welcome.jsx
@@ -6,12 +6,12 @@ export default function Welcome(){
     return(
         <div className={styles.container}>
             <div className={styles.welcome}>
-                <div>
+                <div className={styles.welcome_half}>
                     <h1>Welcome Tech at <span className={styles.include_purple}>#include</span></h1>
                     <br/>
                     <p>We&apos;re so happy to have you on the #include team! This website will be used to aid you in your <b><span className={styles.include_purple}>learning experience</span></b>. Please refer here for important information regarding <b><span className={styles.include_purple}>resources</span></b>, <b><span className={styles.include_purple}>workshops</span></b>, <b><span className={styles.include_purple}>personal bios</span></b>, and <b><span className={styles.include_purple}>contact information</span></b>.</p>
                 </div>
-                <div>
+                <div className={styles.welcome_half}>
                     <Image src="/welcome/bg_logo.svg" alt="bg_logo" width={600} height={600}/>
                 </div>
             </div>
