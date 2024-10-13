c246945cb73736063a65941ae3fb9d9504c0767a
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-10-12 21:58:48 -0700

    feat: Start creating tab layout

--- app/workshop/[num]/page.jsx
+++ app/workshop/[num]/page.jsx
@@ -1,10 +1,25 @@
+'use client';
+
+import { useState } from "react";
 import styles from "./workshop.module.scss";
 
 // params: { num: string }
 export default function Workshop({ params }){
+    const workshopTitles = [[1, "Intro to Web Dev"], [2, "HTML and CSS"], [3, "JavaScript"], [4, "React.js and Next.js"], [5, "APIs and Backend"]];
+    const [num, setNum] = useState(params.num);
+
     return(
-        <div>
-            hello, welcome to week {params.num}!
+        <div className={styles.workshop}>
+            <nav className={styles.workshopNav}>
+                { workshopTitles.map((workshop) => 
+                    <div className={[styles.navTab, (workshop[0] == 1) ? styles.firstNavTab : styles.laterNavTab, (workshop[0] == num)? styles.selectedTab : null].join(' ')} key={workshop[0]}>
+                        <p>{workshop[1]}</p>
+                    </div>
+                ) }
+            </nav>
+            <div>
+                hello, welcome to week {num}! {workshopTitles[num]}
+            </div>
         </div>
     )
 }
