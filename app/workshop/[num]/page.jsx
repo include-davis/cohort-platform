eeb766f9a260122e2f19b6223d83e34fabb09808
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-10-20 15:39:43 -0700

    feat: Implement tab switching

--- app/workshop/[num]/page.jsx
+++ app/workshop/[num]/page.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import styles from "./workshop.module.scss";
+import Link from "next/link";
 
 // params: { num: string }
 export default function Workshop({ params }){
@@ -25,10 +26,10 @@ export default function Workshop({ params }){
                                 {item[1][1] != num && <div className={styles.divider}></div>}
                             </div>
                             :
-                            <div className={[styles.navTab, (item[0] == 1) ? styles.firstNavTab : styles.laterNavTab, (item[0] == num)? styles.selectedTab : null].join(' ')}>
+                            <Link href={`/workshop/${item[0]}`} className={[styles.navTab, (item[0] == 1) ? styles.firstNavTab : styles.laterNavTab, (item[0] == num)? styles.selectedTab : null].join(' ')}>
                                 {item[0] == num && <div className={styles.overlap}></div>}
                                 <p>{item[0]}: {item[1]}</p>
-                            </div>
+                            </Link>
                         }
                     </div>
                 ) }
