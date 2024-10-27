fc40a10772c52d31adf9f682880db02fcb5a13fe
C3viche <28328104+C3viche@users.noreply.github.com>
2024-10-27 13:41:58 -0700

    add timeline component

---
+++ app/_components/weekly-timeline/weekly-timeline.jsx
@@ -0,0 +1,9 @@
+import styles from "@/app/_components/weekly-timeline/weekly-timeline.module.scss"
+
+export default function WeeklyTimeline({ week }) {
+  return (
+    <div className={styles.content}>
+      Hello this is the weekly timeline
+    </div>
+  )
+}
--- app/workshop/[num]/page.jsx
+++ app/workshop/[num]/page.jsx
@@ -1,3 +1,4 @@
+import WeeklyTimeline from "@/app/_components/weekly-timeline/weekly-timeline";
 import styles from "./workshop.module.scss";
 
 // params: { num: string }
@@ -5,6 +6,7 @@ export default function Workshop({ params }){
     return(
         <div>
             hello, welcome to week {params.num}!
+            <WeeklyTimeline week={params.num} />
         </div>
     )
 }
