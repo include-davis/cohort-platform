cdaa97bab4f347c3db877b61cdbe63e11a8f094f
Nandhana Selvam <nandhanaselvam@gmail.com>
2024-10-28 22:10:16 -0700

    Completed try it yourself page

--- app/(homepage)/page.jsx
+++ app/(homepage)/page.jsx
@@ -1,9 +1,12 @@
 import styles from "./homepage.module.scss";
+import TryItOut from "../_components/try-it-out/try-it-out";
+import { defaultCode } from "../_components/try-it-out/default-code";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       Howdy
+      <TryItOut defaultCode={defaultCode} />
     </div>
   );
 }
