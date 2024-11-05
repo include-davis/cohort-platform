4e0e64a32a7673fb12ddb9b83816bc0a4d6686a5
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2024-11-04 22:37:43 -0800

    moved images to public folder

--- app/_components/contact/page.jsx
+++ app/_components/contact/page.jsx
@@ -1,10 +1,5 @@
 import { Individual } from './individual.jsx'
 import styles from './contact.module.scss'
-import Image from 'next/image';
-import Haylie from './images/Haylie.jpg';
-import Michelle from './images/Michelle.jpg';
-import Naomi from './images/Naomi.jpg';
-import Cristian from './images/Cristian.jpg';
 
 export default function Contact(){
     return(
@@ -12,10 +7,10 @@ export default function Contact(){
             <h1>Contacts</h1>
             
             <div className={styles.card_container}>
-                <Individual name="Haylie Tan" image ={Haylie} role="Tech lead" pronouns="she/her" phone="(510)358-1578" email="haytan@ucdavis.edu"/>
-                <Individual name="Haylie Tan" image ={Michelle} role="Tech lead" pronouns="she/her" phone="(510)358-1578" email="haytan@ucdavis.edu"/>
-                <Individual name="Haylie Tan" image ={Naomi} role="Tech lead" pronouns="she/her" phone="(510)358-1578" email="haytan@ucdavis.edu"/>
-                <Individual name="Haylie Tan" image ={Cristian} role="Tech lead" pronouns="she/her" phone="(510)358-1578" email="haytan@ucdavis.edu"/>
+                <Individual name="Haylie Tan" image='/contacts/Haylie.jpg' role="Tech lead" pronouns="she/her" phone="(510)358-1578" email="haytan@ucdavis.edu"/>
+                <Individual name="Haylie Tan" image='/contacts/Michelle.jpg' role="Tech lead" pronouns="she/her" phone="(510)358-1578" email="haytan@ucdavis.edu"/>
+                <Individual name="Haylie Tan" image='/contacts/Naomi.jpg' role="Tech lead" pronouns="she/her" phone="(510)358-1578" email="haytan@ucdavis.edu"/>
+                <Individual name="Haylie Tan" image='/contacts/Cristian.jpg' role="Tech lead" pronouns="she/her" phone="(510)358-1578" email="haytan@ucdavis.edu"/>
             </div>
 
         </div>
