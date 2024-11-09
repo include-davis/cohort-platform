657d74e8b0ccee2cd3a810489cd7375b776fbf7a
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2024-11-09 02:05:55 -0800

    added parts of lessons section

--- app/_components/materials-lessons/materials.jsx
+++ app/_components/materials-lessons/materials.jsx
@@ -9,7 +9,7 @@ export default function Materials({ slideLink, recordingLink }) {
                 <Image src="/navbar/filetab.svg" alt="File Tab Background" className={styles.filetabImage} fill={true} />
             </div>
             <h1>Materials</h1>
-            <p className={styles.description}>Refer to these external websites for more information about how to set up. These links will contain helpful tips and will assist you in gaining a better understanding of development process!</p>
+            <p className={styles.description}>Refer to these workshop materials for this week&apos;s topics. These links will also be a great resource to come back to during the development of the client project!</p>
             <div className={styles.materials}>
                 <div className={styles.link}>
                     <LinkPreview title="Workshop Slides" link={slideLink} theme="light" />
@@ -18,6 +18,11 @@ export default function Materials({ slideLink, recordingLink }) {
                     <LinkPreview title="Workshop Recording" link={recordingLink} theme="light" />
                 </div>
             </div>
+
+            <h1>Lessons</h1>
+            <p className={styles.description}>Here are a some helpful demos for important concepts from this week.</p>
+            {/* TODO: LESSONS SECTION */}
+            
         </div>
     )
 }
