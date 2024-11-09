689971ca691f0eb466b179a9098e3482cce390bb
Anjali Jay Jain <116294517+JayJ104@users.noreply.github.com>
2024-11-08 20:12:37 -0800

    added workshop page welcome section

--- app/workshop/[num]/page.jsx
+++ app/workshop/[num]/page.jsx
@@ -3,39 +3,56 @@
 import { useState } from "react";
 import styles from "./workshop.module.scss";
 import Link from "next/link";
+import WorkshopWelcome from "@/_components/welcome/workshopWelcome";
 
-// params: { num: string }
-export default function Workshop({ params }){
-    const workshopTitles = ["", "Intro to Web Dev", "HTML and CSS", "JavaScript", "React.js and Next.js", "APIs and Backend"];
-    const navItems = []
-    for (let i = 1; i < workshopTitles.length; i ++) {
-        navItems.push([i, workshopTitles[i]])
-        if (i != workshopTitles.length - 1){
-            navItems.push([workshopTitles.length * 2 - i, `d${i}`]);
-        }
+const workshopData = [
+    {
+        title: "Intro to Web Dev",
+        description: "Welcome to your first week! In this week\’s workshop, we will explore web development. Refer to the following videos and links for more information to start!"
+    },
+    {
+        title: "Intro to HTML and CSS",
+        description: "Welcome to your second week! In this week\’s workshop, we will explore HTML and CSS. Refer to the following videos and links for more information to start!"
+    },
+    {
+        title: "JavaScript",
+        description: "Welcome to your third week! In this week\’s workshop, we will explore JavaScript. Refer to the following videos and links for more information to start!"
+    },
+    {
+        title: "React.js and Next.js",
+        description: "Welcome to your fourth week! In this week\’s workshop, we will explore some JS frameworks. Refer to the following videos and links for more information to start!"
+    },
+    {
+        title: "APIs and Backend",
+        description: "Welcome to your fifth week! In this week\’s workshop, we will explore backend development. Refer to the following videos and links for more information to start!"
     }
-    const [num, setNum] = useState(params.num);
+]
+
+export default function Workshop({ params }){
+    const [num, setNum] = useState(params.num-1);
 
     return(
         <div className={styles.workshop}>
+            <WorkshopWelcome title={`${num+1}: ${workshopData[num].title}`} text={workshopData[num].description} />
             <nav className={styles.workshopNav}>
-                { navItems.map((item) => 
-                    <div key={item[0]} className={styles.tabContainer}>
-                        { item[1][0] == "d" ?
-                            <div className={styles.dividerContainer}>
-                                {item[1][1] != num && <div className={styles.divider}></div>}
-                            </div>
-                            :
-                            <Link href={`/workshop/${item[0]}`} className={[styles.navTab, (item[0] == 1) ? styles.firstNavTab : styles.laterNavTab, (item[0] == num)? styles.selectedTab : null].join(' ')}>
-                                {item[0] == num && <div className={styles.overlap}></div>}
-                                <p>{item[0]}: {item[1]}</p>
-                            </Link>
-                        }
+                { workshopData.map((item, idx) => 
+                    <div key={idx} className={styles.tabContainer}>
+                        <Link href={`/workshop/${idx+1}`} 
+                            className={[styles.navTab, (idx == 1) ? styles.firstNavTab : styles.laterNavTab, (idx == num)? styles.selectedTab : null].join(' ')}
+                        >
+                            {idx == num && <div className={styles.overlap}></div>}
+                            <p>{idx+1}: {item.title}</p>
+                        </Link>
+                        <div className={styles.dividerContainer}>
+                            {idx != num && idx!=4 && 
+                                <div className={styles.divider}></div>
+                            }
+                        </div>
                     </div>
                 ) }
             </nav>
             <div>
-                hello, welcome to week {num}! {workshopTitles[num]}
+                hello, welcome to week {num+1}! {workshopData[num].title}
             </div>
         </div>
     )
