3b9e705fc70ca0c5ee928047fb5763b82cdca9bc
Sreya Mathew <ssmathew@ucdavis.edu>
2025-03-16 03:15:55 -0700

    Fixed key linting error

--- app/_demos/map.jsx
+++ app/_demos/map.jsx
@@ -7,9 +7,9 @@ export default function MapDemo(){
         <div className={styles.big_box}>
             <p className={styles.description}>Given an array, apples=["Granny Smith", "Fuji", "Gala"], you can use map to generate an array of UIs for each apple in the array</p>
             <div className={styles.apples}>
-                {apples.map(()=>{
+                {apples.map((apple)=>{
                     return (
-                    <div className={styles.apple}>
+                    <div key={apple} className={styles.apple}>
                         <div className={styles.stem} />
                         <div className={styles.leaf} />
                     </div>
